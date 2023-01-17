79c9740337ee02266bc2f1d9d96d2d7d9cfb1972
ra1lmuz <2003muzafarov@mail.ru>
2023-01-17 14:48:12 +0300

    final-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { useState, useEffect } from "react";
 import TodoHeader from "./components/TodoHeader/TodoHeader";
 import TodoItem from "./components/TodoItem/TodoItem";
 
+const initialTodo = [
+  {
+    id: 1,
+    name: "Купить продукты",
+    date: new Date(),
+    checked: false,
+  },
+  {
+    id: 2,
+    name: "Заправить автомобиль",
+    date: new Date(),
+    checked: false,
+  },
+]
+
 const App = () => {
   // Состояние (данные) задач
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      name: "Купить продукты",
-      date: new Date(),
-      checked: false,
-    },
-    {
-      id: 2,
-      name: "Заправить автомобиль",
-      date: new Date(),
-      checked: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(initialTodo);
 
   return (
     <div className="layout">
